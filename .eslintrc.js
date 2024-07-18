13a502f13ee634db10e8a5f690069ad12f725aac
NiranjanPalani90 <niranjanpalani90@gmail.com>
2024-07-18 09:59:30 +0530

    lint issue resolved

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,10 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'object-curly-newline': 'off',
+    'prefer-arrow-callback': 'off',
+    'comma-dangle': 'off',
+    'color-function-notation': 'off',
+    'alpha-value-notation': 'off'
   },
 };
